20bf3c07aa7f10458251001c18f3ed0ce109887d
SoumavaBanerjee <soumava.rivu@gmail.com>
2021-03-11 20:07:08 +0530

    created button component

--- src/components/searchbar/index.js
+++ src/components/searchbar/index.js
@@ -1,16 +1,74 @@
-import React from "react";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState, useRef } from "react";
+import "./styles.scss";
+import { BiSearchAlt } from "react-icons/bi";
 
 const Searchbar = () => {
+  const [input, setInput] = useState("");
+  const [barOpened, setBarOpened] = useState(false);
+  const formRef = useRef();
+  const inputFocus = useRef();
+
+  const onFormSubmit = (e) => {
+    // When form submited, clear input, close the searchbar and do something with input
+    e.preventDefault();
+    setInput("");
+    setBarOpened(false);
+    // After form submit, do what you want with the input value
+    console.log(`Form was submited with input: ${input}`);
+  };
   return (
-    <div class="search">
-      <input type="text" placeholder="What are you looking for?" />
-      <button type="submit" class="btn__primary">
-        <FontAwesomeIcon icon={faSearch} color="white" />
-      </button>
+    <div className="wrapper">
+      <form
+        className="form"
+        barOpened={barOpened}
+        onClick={() => {
+          // When form clicked, set state of baropened to true and focus the input
+          setBarOpened(true);
+          inputFocus.current.focus();
+        }}
+        // on focus open search bar
+        onFocus={() => {
+          setBarOpened(true);
+          inputFocus.current.focus();
+        }}
+        // on blur close search bar
+        onBlur={() => {
+          setBarOpened(false);
+        }}
+        // On submit, call the onFormSubmit function
+        onSubmit={onFormSubmit}
+        ref={formRef}
+        style={{
+          width: barOpened ? "30rem" : "2rem",
+          cursor: barOpened ? "auto" : "pointer",
+        }}
+      >
+        <button
+          style={{
+            pointerEvents: barOpened ? "auto" : "none",
+            cursor: barOpened ? "pointer" : "none",
+          }}
+          className="search__btn"
+          type="submit"
+          barOpened={barOpened}
+        >
+          <BiSearchAlt />
+        </button>
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          className="searchbar"
+          ref={inputFocus}
+          value={input}
+          barOpened={barOpened}
+          placeholder="Search..."
+          style={{ marginLeft: barOpened ? "1rem" : "0rem" }}
+        />
+      </form>
     </div>
   );
 };
 
 export default Searchbar;
+//   <div className="submitsearch">
+// <span>Search</span>
+// </div>
